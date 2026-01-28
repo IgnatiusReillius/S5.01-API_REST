34b4ba527c19e3d549fbe5e927a94ea471b8c7b5
IgnatiusReillius <ignatiusreillius@hotmail.com>
2026-01-28 17:58:16 +0100

    TDD red: list user by id

--- tests/Feature/User/UserReadTest.php
+++ tests/Feature/User/UserReadTest.php
@@ -11,11 +11,25 @@ class UserReadTest extends TestCase
     use RefreshDatabase;
 
     public function test_can_list_users() : void {
+
         User::factory()->count(2)->create();
         
         $response = $this->getJson('/api/users');
         
         $response->assertStatus(200)
                  ->assertJsonCount(2, 'data'); 
+    }
+
+    public function test_can_show_user_by_id() : void {
+        
+        $user = User::factory()->create();
+        
+        $response = $this->getJson("/api/users/{$user->id}");
+        
+        $response->assertStatus(200)
+                 ->assertJsonFragment([
+                     'id' => $user->id,
+                     'email' => $user->email                      
+                 ]);
     }
 }
