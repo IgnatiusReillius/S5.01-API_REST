6ddbe3e8e747fe38af66c19ca487e8b40b6d0ad2
IgnatiusReillius <ignatiusreillius@hotmail.com>
2026-01-28 17:30:49 +0100

    TDD red: list users

---
+++ tests/Feature/User/UserReadTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserReadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_can_list_users() : void {
+        User::factory()->count(2)->create();
+        
+        $response = $this->getJson('/api/users');
+        
+        $response->assertStatus(200)
+                 ->assertJsonCount(3); 
+    }
+}
