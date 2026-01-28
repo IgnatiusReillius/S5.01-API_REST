c4922c70a6744fef855d76c5234cd222fc1862e6
IgnatiusReillius <ignatiusreillius@hotmail.com>
2026-01-28 17:14:08 +0100

    TDD green: create user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function store(Request $request) : JsonResponse {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        
+        return response()->json(['data' => $user], 201);
+    }
+}
