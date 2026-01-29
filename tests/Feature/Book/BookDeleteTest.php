4cd7fe2f111cec5d03ac8b7be4f9f3e7a749735e
IgnatiusReillius <ignatiusreillius@hotmail.com>
2026-01-29 12:48:37 +0100

    TDD green: non user and unauthenticated user cannot delete book

--- tests/Feature/Book/BookDeleteTest.php
+++ tests/Feature/Book/BookDeleteTest.php
@@ -13,7 +13,7 @@ class BookDeleteTest extends TestCase
     use RefreshDatabase;
 
     public function test_admin_can_delete_specific_book() : void {
-
+        
         $admin = User::factory()->admin()->create();
         
         $book = Book::factory()->create();
@@ -25,6 +25,36 @@ class BookDeleteTest extends TestCase
         $response->assertStatus(204);
 
         $this->assertDatabaseMissing('books', [
+            'id' => $book->id,
+        ]);
+    }
+
+    public function test_non_admin_cannot_delete_book() : void {
+
+        $user = User::factory()->create();
+        
+        $book = Book::factory()->create();
+
+        Passport::actingAs($user);
+
+        $response = $this->deleteJson("/api/books/{$book->id}");
+
+        $response->assertStatus(403);
+
+        $this->assertDatabaseHas('books', [
+            'id' => $book->id,
+        ]);
+    }
+
+    public function test_unauthenticated_user_cannot_delete_book() : void {
+
+        $book = Book::factory()->create();
+
+        $response = $this->deleteJson("/api/books/{$book->id}");
+
+        $response->assertStatus(401);
+
+        $this->assertDatabaseHas('books', [
             'id' => $book->id,
         ]);
     }
