e214c817138ff193a3da9bac3f859c4660ad7a85
IgnatiusReillius <ignatiusreillius@hotmail.com>
2026-01-29 05:51:40 +0100

    TDD green: user car register and recieve token

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
+    public function register(UserRequest $request) : JsonResponse {
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('access_token')->accessToken;
+
+        return response()->json([
+            'data' => $user,
+            'access_token' => $token,
+            ], 201);
+    }
+
     public function login(LoginRequest $request) : JsonResponse {
 
         if (!Auth::attempt($request->only('email', 'password'))) {
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthenticationController::class, 'login']);
-
+Route::post('/register', [AuthenticationController::class, 'register']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthenticationController::class, 'logout']);
