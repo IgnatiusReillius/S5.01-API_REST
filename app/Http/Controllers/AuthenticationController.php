0c22ef342060e327423ca47aa3eb39e305b79abe
IgnatiusReillius <ignatiusreillius@hotmail.com>
2026-01-29 03:40:33 +0100

    TDD green: cannot login with invalid data (several tests)

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 
 class AuthenticationController extends Controller
 {
-    public function login(Request $request) : JsonResponse {
+    public function login(LoginRequest $request) : JsonResponse {
 
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json(['message' => 'Invalid credentials'], 401);
