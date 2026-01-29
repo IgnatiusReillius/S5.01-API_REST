61b279285baf3b0505024f2443f66739a37b642d
IgnatiusReillius <ignatiusreillius@hotmail.com>
2026-01-29 21:57:28 +0100

    TDD green: user can update own review

---
+++ app/Http/Requests/ReviewUpdateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReviewUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'read_date' => 'nullable|date',
+            'comment' => 'nullable|string|max:1000',
+            'rating' => 'sometimes|required|integer|min:1|max:5',
+        ];
+    }
+}
